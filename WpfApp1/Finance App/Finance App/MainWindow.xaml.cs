35de8f6c1468bf67833998dfe0d584082ba7158b
Ashen Hiroshana <64160989+AshenHiroshana@users.noreply.github.com>
2022-03-02 19:58:48 +0530

    test k

---
+++ WpfApp1/Finance App/Finance App/MainWindow.xaml.cs
@@ -0,0 +1,115 @@
+﻿using Finance_App.View;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Finance_App
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ListView_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            ListBoxItem lbi = ((sender as ListBox).SelectedItem as ListBoxItem);
+            string menuItem = lbi.Name.ToString();
+
+            if(menuItem == "Home")
+                DataContext = new HomeView();
+            if (menuItem == "Income")
+                DataContext = new IncomeView();
+
+        }
+
+
+        private void dragMe(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                DragMove();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void closeApp(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                Close();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void closeHoverON(object sender, MouseEventArgs e)
+        {
+            if (sender is Ellipse ellipse)
+            {
+                BrushConverter converter = new BrushConverter();
+                ellipse.Fill = converter.ConvertFromString("#F33") as Brush;
+            }
+        }
+
+        private void closeHoverOFF(object sender, MouseEventArgs e)
+        {
+            if (sender is Ellipse ellipse)
+            {
+                BrushConverter converter = new BrushConverter();
+                ellipse.Fill = converter.ConvertFromString("#D00") as Brush;
+            }
+        }
+
+
+        private void minimizeApp(object sender, MouseButtonEventArgs e)
+        {
+            try
+            {
+                this.WindowState = WindowState.Minimized;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+        }
+
+        private void minimizeHoverON(object sender, MouseEventArgs e)
+        {
+            if (sender is Ellipse ellipse)
+            {
+                BrushConverter converter = new BrushConverter();
+                ellipse.Fill = converter.ConvertFromString("#FF3") as Brush;
+            }
+        }
+
+        private void minimizeHoverOFF(object sender, MouseEventArgs e)
+        {
+            if (sender is Ellipse ellipse)
+            {
+                BrushConverter converter = new BrushConverter();
+                ellipse.Fill = converter.ConvertFromString("#DD0") as Brush;
+            }
+        }
+    }
+}
---
+++ WpfApp1/Finance App/Finance App/View/IncomeView.xaml.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Finance_App.View
+{
+    /// <summary>
+    /// Interaction logic for IncomeView.xaml
+    /// </summary>
+    public partial class IncomeView : UserControl
+    {
+        public IncomeView()
+        {
+
+            List<List<int>> lsts = new List<List<int>>();
+
+            for (int i = 0; i < 5; i++)
+            {
+                lsts.Add(new List<int>());
+
+                for (int j = 0; j < 2; j++)
+                {
+                    lsts[i].Add(i * 10 + j);
+                }
+            }
+
+            InitializeComponent();
+
+            lst.ItemsSource = lsts;
+            
+
+
+
+
+        }
+
+        private void Next(object sender, RoutedEventArgs e)
+        {
+            IncomeForm1.Visibility = Visibility.Hidden;
+            IncomeForm2.Visibility = Visibility.Visible;
+        }
+
+        private void Prev(object sender, RoutedEventArgs e)
+        {
+            IncomeForm2.Visibility = Visibility.Hidden;
+            IncomeForm1.Visibility = Visibility.Visible;
+        }
+    }
+}
